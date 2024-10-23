6c5d52cd43a93391dbcddf4bf9929552d7aa0e2f
takatsutsu <vivehodie3163@gmail.com>
2024-10-24 01:27:03 +0900

    mypageの画像取得部分修正。リレーション部分の複数形への変更

--- src/app/Http/Controllers/MyPageController.php
+++ src/app/Http/Controllers/MyPageController.php
@@ -2,14 +2,17 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Shop;
+use App\Models\Reservation;
 
 class MyPageController extends Controller
 {
     public function my_page()
     {
         $user = Auth::user();
-        $reservations = $user->reservation()->with('shop')->get();
-        $favorites = $user->favorite()->with('shop')->get();
+        $reservations = $user->reservations()->with('shop')->get();
+        $favorites = $user->favorites()->with('shop')->get();
 
         return view('my_page', compact('user', 'reservations', 'favorites'));
     }
